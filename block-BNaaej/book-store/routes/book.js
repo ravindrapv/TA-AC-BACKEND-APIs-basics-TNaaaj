ae168797182715c44f8342682ab00169e5eccd7c
ravindrapv <ravindra.ravi4849@gmail.com>
2022-10-04 14:25:31 +0530

    book-store-api

---
+++ block-BNaaej/book-store/routes/book.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var Book = require('../models/book');
+
+//list all books
+router.get('/', (req, res, next) => {
+  Book.find({}, (err, books) => {
+    if (err) return next(err);
+    res.status(200).json({ books });
+  });
+});
+
+//list a single book
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Book.findById(id, (err, book) => {
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+//create a book
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  req.body.tags = req.body.tags.trim().split(',');
+  Book.create(req.body, (err, book) => {
+    console.log(err, book);
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+//edit a book
+router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  req.body.tags = req.body.tags.trim().split(',');
+  Book.findByIdAndUpdate(id, req.body, (err, book) => {
+    if (err) return next(err);
+    console.log(book);
+    res.status(200).json({ book });
+  });
+});
+
+//delete a book
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Book.findByIdAndDelete(id, (err, book) => {
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+module.exports = router;
